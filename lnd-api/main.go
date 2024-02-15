e82e55caa0e6314fe0ac7b3830a217392fdb5706
Fordjour12 <fordjourbobiem@gmail.com>
2024-02-15 18:46:27 +0000

    Refactor Person struct and add AddHandler

--- lnd-api/main.go
+++ lnd-api/main.go
@@ -7,12 +7,18 @@ import (
 )
 
 type Person struct {
-	Name string
-	Age  int
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
+var (
+	items  = []Person{}
+	NextID = 1
+)
+
 func GoHandler(w http.ResponseWriter, r *http.Request) {
-	p := Person{Name: "John", Age: 25}
+	p := Person{Name: "John", Age: 25, Id: 1}
 
 	// Encode the struct to JSON and write it to the response(w.WriteHeader(http.StatusOK)is not needed)
 	// err := json.NewEncoder(w).Encode(p)
@@ -29,10 +35,25 @@ func GoHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func AddHandler(w http.ResponseWriter, r *http.Request) {
+	var p Person
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	p.Id = NextID
+	NextID++
+
+	ps := append(items, p)
+	err = json.NewEncoder(w).Encode(ps)
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /", GoHandler)
+	mux.HandleFunc("POST /add", AddHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
